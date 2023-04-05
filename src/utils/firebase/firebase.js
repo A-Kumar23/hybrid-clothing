f7be33d93e3bebeb6e68bd2a1f637f29dc23bf85
Arun Kanaujia <Arun.kanojia@team.cliffex.com>
2023-04-05 15:42:57 +0530

    firebase google signin

--- src/utils/firebase/firebase.js
+++ src/utils/firebase/firebase.js
@@ -1,7 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider} from 'firebase/auth'
-
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { getFirestore, doc, setDoc, getDoc } from "firebase/firestore";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDi9JExRB9FSD7O5SBqxGebhp48taN9Ljs",
@@ -9,17 +14,45 @@ const firebaseConfig = {
   projectId: "hybrid-clothing-db",
   storageBucket: "hybrid-clothing-db.appspot.com",
   messagingSenderId: "839211030030",
-  appId: "1:839211030030:web:2b47b5fe8ed534d830ce5f"
+  appId: "1:839211030030:web:2b47b5fe8ed534d830ce5f",
 };
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider(); 
+const provider = new GoogleAuthProvider();
 
 provider.setCustomParameters({
-    prompt: "select_account"
+  prompt: "select_account",
 });
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log(email);
+    }
+  }
+
+  return userDocRef;
+};
