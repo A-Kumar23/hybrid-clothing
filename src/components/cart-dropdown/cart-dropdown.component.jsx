6e14760cf58a1a235e9abca413cd1c618f916460
Arun Kanaujia <Arun.kanojia@team.cliffex.com>
2023-04-27 14:39:53 +0530

    add new functlinity

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import './cart-dropdown.style.scss'
 import Button from "../button/button.componrnt";
+import CartItem from "../cart-item/cart-item.component";
+import { CartContext } from "../../contexts/cart.context";
+
+
+
 const  CartDropdown = () =>{
+
+    const {cartItems} = useContext(CartContext)
+    console.log(cartItems);
     return(
         <div className="cart-dropdown-container">
-            <div className="cart-item"></div>
+            <div className="cart-items">
+                {
+                    
+                    cartItems.map((item) => <CartItem key={item.id} cartItem={item}/>)
+                }
+            </div>
             <Button>Go to checkout</Button>
         </div>
     )
--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import './product-card.styles.scss'
 import Button from "../button/button.componrnt";
+import { CartContext } from "../../contexts/cart.context";
 
 const ProductCard = ({product}) => {
     const {name, price, imageUrl} = product;
+
+    const addProductToCart = () => addItemToCart(product)
+    const { addItemToCart } = useContext(CartContext)
     return (
         <div className="product-card-container">
         <img src={imageUrl} alt={name}/>
@@ -11,7 +15,7 @@ const ProductCard = ({product}) => {
             <span className="name">{name}</span>
             <span className="price">{price}$</span>
         </div>
-            <Button buttonType='inverted' >Add to cart</Button>
+            <Button buttonType='inverted' onClick={addProductToCart} >Add to cart</Button>
         </div>
     )
 }
